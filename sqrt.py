8b56926f06ad397b34ce9a7905e5b82c53002c7f
3bod <77662851+3bod-5@users.noreply.github.com>
2021-10-28 21:44:36 +0200

    Add files via upload

--- sqrt.py
+++ sqrt.py
@@ -1,15 +1,18 @@
-from math import sqrt
 def square(x=0,y=0):
-
         for i in range(x,y+1):
+            mylist=[]
             num=int(i*i)
-            if (num>x and num<y):
-                print (num)
+            mylist.append(num)
+            if(num>x and num<y):
+                print(mylist , end=" , ")
+
 
 
 x=int(input("enter frist number : "))
 y=int(input("enter second number : "))
 square(x,y)
+
+
 
 
 
